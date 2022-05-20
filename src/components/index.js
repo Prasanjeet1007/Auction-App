f7a5edc5446e09359bbdff6e8c18ea8fb5f39609
Prasanjeet1007 <pkkaswan1007@gmail.com>
2022-05-20 18:34:07 +0530

    Added my project

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./Home";
+export { default as About } from "./ForDealer";
+export { default as Contact } from "./ForOwner";
+export { default as Blog } from "./whyUseApp/Blog";
+export { default as Posts } from "./whyUseApp/Posts";
+export { default as Post } from "./whyUseApp/Post";
---
+++ src/components/whyUseApp/Post.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
+
+function Post() {
+  let { postSlug } = useParams();
+
+  useEffect(() => {
+    // Fetch post using the postSlug
+  }, [postSlug]);
+
+  return (
+    <div className="home">
+      <div class="container">
+        <h1 className="mt-5">This is a Post Title</h1>
+        <h6 className="mb-5">The post slug is, {postSlug}</h6>
+        <p>
+         
+       
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
